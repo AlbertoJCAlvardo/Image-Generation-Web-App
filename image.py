87380f31aed1f65facfff691dc30fa9c66a3128f
Alberto Alvarado <85086650+AlbertoJCAlvardo@users.noreply.github.com>
2023-11-11 18:49:54 -0600

    Add files via upload

--- image.py
+++ image.py
@@ -18,27 +18,32 @@ def index():
 def generar_imagen():
 
     prompt = request.form['prompt']
+    ancho = int(request.form['ancho'])
+    alto = int(request.form['alto'])
 
+    dimensiones = [ '1024x1024', '1024x1792', '1792x1024'] 
 
-    response = client.images.generate(
-    model="dall-e-3",
-    prompt= prompt,
-    size="1024x1024",
-    quality="standard",
-    n=1,
-    response_format = 'b64_json'
-    )
+    if ancho>0 and alto>0 and f"{ancho}x{alto}" in dimensiones:
+        response = client.images.generate(
+        model="dall-e-3",
+        prompt= prompt,
+        size=f"{ancho}x{alto}",
+        quality="standard",
+        n=1,
+        response_format = 'b64_json'
+        )
 
-    b64_string = response.data[0].b64_json
+        b64_string = response.data[0].b64_json
 
-    decoded = base64.b64decode(b64_string)
+        decoded = base64.b64decode(b64_string)
 
-    image = Image.open(BytesIO(decoded)).convert("RGB")    
-    fpath = 'static/imagenes/result.jpg'
-    image.save(fpath, "jpeg")
+        image = Image.open(BytesIO(decoded)).convert("RGB")    
+        fpath = 'static/imagenes/result.jpg'
+        image.save(fpath, "jpeg")
     
-    return render_template('resultado.html' )
-
+        return render_template('resultado.html' )
+    else:
+        return "Error!"
 
 if __name__ == '__main__':
     app.run(debug=True)
